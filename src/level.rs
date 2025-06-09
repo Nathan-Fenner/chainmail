3e1c0e4806253a7ebc5f96d15328ce4667ed9346
suann-c <c.sueey@gmail.com>
2025-06-08 18:16:34 -0700

    spawn chain works, game is getting stuck elsewhere

--- src/level.rs
+++ src/level.rs
@@ -1,5 +1,5 @@
-use std::sync::Mutex;
 use std::collections::VecDeque;
+use std::sync::Mutex;
 
 use avian3d::prelude::*;
 use bevy::{
@@ -159,7 +159,7 @@ fn load_level_system(
     // If there is no player, load the first level.
 
     if !*has_loaded_player {
-        let first_level = LevelName::from_string("level_1.png".to_string());
+        let first_level = LevelName::from_string("level_6.png".to_string());
         load_level(
             Vec3::ZERO,
             LevelTag {
@@ -1083,6 +1083,7 @@ fn load_level(
     }
 
     spawn_chains(&level_tag, commands, common, &chains.lock().unwrap());
+    println!("spawn chains done");
 }
 
 fn spawn_ziplines(
@@ -1182,33 +1183,30 @@ fn spawn_chain<'a>(
     chain_positions: &HashMap<IVec2, Vec3>,
     starting_chain_ball: (IVec2, Vec3),
     visited: &'a mut HashMap<IVec2, bool>,
-) -> (Vec<(IVec2, Vec3)>)
-{
+) -> (Vec<(IVec2, Vec3)>) {
     //valid chain dirs
-    let dirs= [
-        ivec2(0, 1),
-        ivec2(0, -1),
-        ivec2(1, 0),
-        ivec2(-1, 0)
-    ];
-    let mut chain_list: Vec<(IVec2, Vec3)>=Vec::new();
-    let mut chain_q=VecDeque::new();
+    let dirs = [ivec2(0, 1), ivec2(0, -1), ivec2(1, 0), ivec2(-1, 0)];
+    let mut chain_list: Vec<(IVec2, Vec3)> = Vec::new();
+    let mut chain_q = VecDeque::new();
     chain_q.push_back(starting_chain_ball);
 
     //bfs
-    while let Some(curr_chain) = chain_q.pop_front(){
-        let curr_chain_ball= curr_chain.0;
+    while let Some(curr_chain) = chain_q.pop_front() {
+        let curr_chain_ball = curr_chain.0;
         chain_list.push(curr_chain);
-        for d in dirs{
-            let next_chain_key=ivec2(curr_chain_ball.x+d[0], curr_chain_ball.y+d[1]); //potential same chain
-            if chain_positions.contains_key(&next_chain_key) && !visited.contains_key(&next_chain_key){ //validate
+        for d in dirs {
+            let next_chain_key = ivec2(curr_chain_ball.x + d[0], curr_chain_ball.y + d[1]); //potential same chain
+            if chain_positions.contains_key(&next_chain_key)
+                && !visited.contains_key(&next_chain_key)
+            {
+                //validate
                 chain_q.push_back((next_chain_key, chain_positions[&next_chain_key]));
             };
         }
         visited.insert(curr_chain_ball, true);
     }
 
-    return chain_list
+    return chain_list;
 }
 
 fn spawn_chains(
@@ -1217,6 +1215,7 @@ fn spawn_chains(
     common: &Common,
     chain_positions: &HashMap<IVec2, Vec3>,
 ) {
+    println!("spawn chains {:?}", chain_positions);
     let mut chain_entities: HashMap<IVec2, Entity> = default();
 
     let mut chain_ends: Vec<(Entity, Vec3)> = Vec::new();
@@ -1226,14 +1225,22 @@ fn spawn_chains(
 
     for (chain_ball, chain_pos) in chain_positions.iter() {
         //call spawn chain when encountering and unvisited chainball
-        if !visited.contains_key(chain_ball){
+        if !visited.contains_key(chain_ball) {
             let chain_loc = spawn_chain(chain_positions, (*chain_ball, *chain_pos), &mut visited);
             chain_locs.push(chain_loc);
         }
     }
 
-    for chain_loc in chain_locs{
-        for (chain_ball, chain_pos) in chain_loc{
+    if chain_locs.len()>=2{
+        for chain in chain_locs{
+            spawn_chains(level_tag, commands, common, &chain.into_iter().collect());
+        }
+        return
+    }
+
+    for chain_loc in chain_locs {
+        println!("this is a chain");
+        for (chain_ball, chain_pos) in chain_loc {
             let collision_layer = if (chain_ball.x + chain_ball.y) % 2 == 0 {
                 let mut interact = LayerMask::ALL;
                 interact.remove(4);
@@ -1244,26 +1251,27 @@ fn spawn_chains(
                 CollisionLayers::new(4, interact)
             };
             let chain_id = commands
-            .spawn((
-                level_tag.clone(),
-                Mesh3d(common.mesh_small_sphere.clone()),
-                MeshMaterial3d(common.material_dark_gray.clone()),
-                Transform::from_translation(chain_pos).with_scale(Vec3::splat(0.75)),
-                RigidBody::Dynamic,
-                ColliderDensity(0.1),
-                Collider::sphere(0.5),
-                collision_layer,
-            ))
-            .id();
-        if [IVec2::X, IVec2::NEG_X, IVec2::Y, IVec2::NEG_Y]
-        .into_iter()
-        .filter(|d| chain_positions.contains_key(&(chain_ball + *d)))
-        .count()
-        == 1
-        {
-            chain_ends.push((chain_id, chain_pos));
-        }
-        chain_entities.insert(chain_ball, chain_id);
+                .spawn((
+                    level_tag.clone(),
+                    Mesh3d(common.mesh_small_sphere.clone()),
+                    MeshMaterial3d(common.material_dark_gray.clone()),
+                    Transform::from_translation(chain_pos).with_scale(Vec3::splat(0.75)),
+                    RigidBody::Dynamic,
+                    ColliderDensity(0.1),
+                    Collider::sphere(0.5),
+                    collision_layer,
+                ))
+                .id();
+            if [IVec2::X, IVec2::NEG_X, IVec2::Y, IVec2::NEG_Y]
+                .into_iter()
+                .filter(|d| chain_positions.contains_key(&(chain_ball + *d)))
+                .count()
+                == 1
+            {
+                println!("These are chain ends {:?}", (chain_id, chain_pos));
+                chain_ends.push((chain_id, chain_pos));
+            }
+            chain_entities.insert(chain_ball, chain_id);
         }
     }
 
